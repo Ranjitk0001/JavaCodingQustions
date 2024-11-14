030644b785b63df32bdd72e03c574871896c01d1
rk <ranjitkale0001@gmail.com>
2024-11-14 15:18:44 +0530

    Longest SubString Without Repeating Char

---
+++ src/string/LongestSubStringWithoutRepeatingChar.java
@@ -0,0 +1,40 @@
+package string;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class LongestSubStringWithoutRepeatingChar {
+
+	public static void main(String[] args) {
+		LengthOfLongestSubString("abbac"); 
+
+	}
+	
+	public static void LengthOfLongestSubString(String s) {
+		String LongestSubString =null;
+		int LongestSubStringLength=0;
+		Map<Character,Integer>map=new LinkedHashMap<>();
+		
+		char arr[]=s.toCharArray();
+		for(int i=0;i<arr.length;i++) {
+			char ch=arr[i];
+			
+			if(!map.containsKey(ch)) {
+				map.put(ch,i);//a=0 b=1 c=2
+			}
+			else {
+				i=map.get(ch);
+				map.clear();
+			}
+			
+		  if(map.size() > LongestSubStringLength) {
+			  LongestSubStringLength=map.size();
+			  LongestSubString=map.keySet().toString();
+		  }	
+		}
+		
+		System.out.println("The Longest subString :-"+ LongestSubString);
+		System.out.println("The Longest subString Length:"+LongestSubStringLength);
+	}
+
+}
